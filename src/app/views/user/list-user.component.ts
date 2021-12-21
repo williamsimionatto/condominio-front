635377ae756a227198a5b2eb92e73c76061cbef4
William <178344@upf.br>
2021-12-21 16:40:09 -0300

    feat: add table for all users

--- src/app/views/user/list-user.component.ts
+++ src/app/views/user/list-user.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './list-user.component.html'
+  templateUrl: './list-user.component.html',
+  styleUrls: ['../../../assets/css/default.scss']
 })
 export class ListUserComponent implements OnInit {
   constructor() { }
