74be8c8dbbd13c0d338deacfa0a2d176098dace0
William <178344@upf.br>
2022-03-02 20:16:07 -0300

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/angular-app-template'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+ '/dist/angular-app-template/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
