531e923402b218cd3c1a2f6e5112cb8edb0f63e5
William <178344@upf.br>
2022-03-06 19:14:57 -0300

    sei lá 3

--- src/app/views/leitura-agua/leitura-agua-routing.module.ts
+++ src/app/views/leitura-agua/leitura-agua-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AddEditLeituraAguaComponent } from "./add/add.component";
 import { ListLeituraAguaComponent } from "./list/list.component";
-import { OverviewLeituraAguaComponent } from "./overview/overview-perfil.component";
 
 const routes: Routes = [
   { path: "", component: ListLeituraAguaComponent },
   { path: "add", component: AddEditLeituraAguaComponent},
   { path: "edit/:id", component: AddEditLeituraAguaComponent},
-  { path: "overview/:id", component: OverviewLeituraAguaComponent}
 ]
 
 @NgModule({
--- src/app/views/leitura-agua/leitura-agua.module.ts
+++ src/app/views/leitura-agua/leitura-agua.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ChartsModule } from 'ng2-charts';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { LeituraAguaRoutingModule } from './leitura-agua-routing.module';
