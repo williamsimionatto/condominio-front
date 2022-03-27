08da1d095c1a37dfd1e76e36b8634e34e97496ab
William <178344@upf.br>
2022-03-27 20:08:57 -0300

    feat: create card-header component

--- src/app/containers/shared.module.ts
+++ src/app/containers/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { ModuleWithProviders, NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CardHeaderComponent } from "./card-header/card-header.component";
 import { InputComponent } from "./input";
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, CardHeaderComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [InputComponent, CommonModule, FormsModule, ReactiveFormsModule ],
+  exports: [InputComponent, CardHeaderComponent, CommonModule, FormsModule, ReactiveFormsModule ],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders<SharedModule> {
