a19bfd43a6f1915f2f84e1683b991a8b7ae7d896
William <178344@upf.br>
2022-04-21 15:40:53 -0300

    feat: create leituraagua report module

---
+++ src/app/views/report/leituraagua/leitura-agua-report-module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "../../../containers/shared.module";
+import { LeituraAguaReportRoutingModule } from "./leitura-agua-report-routing.module";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LeituraAguaReportRoutingModule,
+    SharedModule
+  ],
+  declarations: [
+  ],
+})
+export class LeituraAguaReportModule {
+}
