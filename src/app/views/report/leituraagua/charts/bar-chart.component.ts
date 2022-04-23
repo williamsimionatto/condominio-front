51a194eb60110d2059a6b5ce35808f40c848aaf0
William <178344@upf.br>
2022-04-23 16:42:27 -0300

    feat: sumarize data

--- src/app/views/report/leituraagua/charts/bar-chart.component.ts
+++ src/app/views/report/leituraagua/charts/bar-chart.component.ts
@@ -10,7 +10,7 @@ import { LocalStorageService } from "../../../../service";
   styleUrls: ['../../../../../assets/css/default.scss']
 })
 export class BarChartCompoent implements OnInit {
-  @Input() chartData: LeituraAguaReportParams
+  @Input() chartData: LeituraAguaReportParams[]
 
   public user: UserParamsAuth;
   constructor(
@@ -22,14 +22,37 @@ export class BarChartCompoent implements OnInit {
     this.prepareChartData(this.chartData)
   }
 
-  prepareChartData(data: LeituraAguaReportParams) {
+  prepareChartData(data: LeituraAguaReportParams[]) {
     if (this.user.perfil.sigla === 'ADMIN' || this.user.perfil.sigla === 'SIND') {
-      this.prepareSindicatoChartData(data)
+      this.prepareSindicoChartData(data)
     }
   }
 
-  prepareSindicatoChartData(data: LeituraAguaReportParams) {
-    console.log(data)
+  groupBy(xs, key: string): LeituraAguaReportParams[] {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+
+  prepareSindicoChartData(data: LeituraAguaReportParams[]) {
+    let series: SerieParams[] = []
+    let sortedData = data.sort((a, b) => {
+      return a.dataleitura > b.dataleitura ? 1 : -1
+    })
+
+    let groupedData = this.groupBy(sortedData, 'dataleitura')
+    const keys = Object.keys(groupedData);
+
+    keys.forEach(key => {
+      const serieData = groupedData[key]
+      const consumo = serieData.reduce((a, b) => a + parseInt(b.consumo), 0)
+      series.push({
+        type: undefined,
+        name: key,
+        data: [consumo]
+      })
+    })
   }
 
   barChart() {
@@ -80,4 +103,10 @@ export class BarChartCompoent implements OnInit {
     });
   }
 
+}
+
+export type SerieParams = {
+  type: string,
+  name: string,
+  data: number[]
 }
