0752eb8bd2bcfd5f4b75f3b430fd6ef6b30e6e72
William <178344@upf.br>
2022-04-24 17:51:48 -0300

    feat: Add password change option in logout dropdown

--- src/app/views/user/password/password.component.ts
+++ src/app/views/user/password/password.component.ts
@@ -77,7 +77,7 @@ export class PasswordComponent implements OnInit {
       .pipe(first())
       .subscribe({
         next: () => {
-          this.router.navigate(['/usuario'], { relativeTo: this.route });
+          this.router.navigate(['/']);
           this.notificationService.showSuccess('Senha Atualizada com sucesso!', 'Sucesso');
         },
         error: ret => {
@@ -98,6 +98,7 @@ export class PasswordComponent implements OnInit {
       .verifyPassword(data)
       .pipe(first())
       .subscribe((x: any) => {
+        console.log(x)
         if (x.ok) {
           this.loading = true
           this.updatePassword()
