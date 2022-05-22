2dc890c6e5e0c4f95703927236b23cf19815cb3c
William <178344@upf.br>
2022-05-22 15:03:17 -0300

    test: apply e2e teste into Authentication Use Case

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,7 +1,36 @@
 /// <reference types="Cypress" />
 
 describe('Authetication Use Case', () => {
-    it('Login', () => {
-        cy.visit('www.google.com')
-    })
+	beforeEach(() => {
+		cy.visit('/login')
+	})
+
+	it('should rendered login page', () => {
+		cy.get('h1').should('contain', 'Login')
+	})
+
+	it('should login successfully', () => {
+		cy.get('#email')
+			.should('be.visible')
+			.type(Cypress.env('localUser').email, { log: false, force: true })
+
+		cy.get('#password')
+			.should('be.visible')
+			.type(Cypress.env('localUser').password, { log: false, force: true })
+
+		cy.contains('button', 'Acessar').should('be.visible').click({ force: true })
+	})
+
+	it('should prevent login with invalid credentials', () => {
+		cy.get('#email')
+			.should('be.visible')
+			.type('mail@mail.com', { log: false, force: true })
+
+		cy.get('#password')
+			.should('be.visible')
+			.type('senha', { log: false, force: true })
+
+		cy.contains('button', 'Acessar').should('be.visible').click({ force: true })
+		cy.contains('h1', 'Login').should('be.visible')
+	})
 })
